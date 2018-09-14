6d954aff9f23c3543b82765467c1dda8d5e3e995
DESKTOP-TTUSLQH\Mogami <atsuyatsuchikawa@gmail.com>
2018-09-14 11:52:56 +0900

    first commit

---
+++ Koyomin2/Koyomin2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Koyomin2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.プロジェクトToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ビルドToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.プロジェクトToolStripMenuItem,
+            this.ビルドToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // プロジェクトToolStripMenuItem
+            // 
+            this.プロジェクトToolStripMenuItem.Name = "プロジェクトToolStripMenuItem";
+            this.プロジェクトToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.プロジェクトToolStripMenuItem.Text = "プロジェクト";
+            // 
+            // ビルドToolStripMenuItem
+            // 
+            this.ビルドToolStripMenuItem.Name = "ビルドToolStripMenuItem";
+            this.ビルドToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.ビルドToolStripMenuItem.Text = "ビルド";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Koyoin2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem プロジェクトToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ビルドToolStripMenuItem;
+    }
+}
+
